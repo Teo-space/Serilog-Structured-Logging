6078613e327440f6ea955d620cb54b785952cde8
b.a.n <barmin.andrey.nnov@gmail.com>
2023-10-21 13:23:23 +0300

    up

--- Serilog Structured Logging/Program.cs
+++ Serilog Structured Logging/Program.cs
@@ -1,5 +1,7 @@
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 using Serilog_Structured_Logging;
+
 
 IHost host = Host.CreateDefaultBuilder(args)
 .ConfigureServices(services =>
@@ -11,28 +13,32 @@ IHost host = Host.CreateDefaultBuilder(args)
 .ReadFrom.Configuration(context.Configuration)
 .ReadFrom.Services(services)
 .Enrich.FromLogContext()
-.WriteTo.Console())
-.Build();
+.WriteTo.Console()
 
+.WriteTo.MongoDBBson("mongodb://127.0.0.1:27017/logs")
+
+//docker run --name influxdb -p 8086:8086 influxdb:latest
+// username/pass influxdb
+// generate token in web interface using username/pass
+//Create organization/bucket, get organization id
+.WriteTo.InfluxDB(applicationName: "Quick test",
+                uri: new Uri("http://127.0.0.1:8086"),
+                organizationId: "5b5d545e92a59039",
+                bucketName: "logs",
+                token: "7Jje9xYjaV5rhqnuLYrnSVyJjHRM2kLg7Fivec8sLli2nKp3svpeOa1AleHb657MuV-Ft0jkwMElqfyjVhkkdg==")
+
+//docker network create elastic
+//docker run - d--name elasticsearch--net elastic - p 9200:9200 - p 9300:9300 - e "discovery.type=single-node" elasticsearch: 8.10.2
+.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200")))
+
+
+
+)
+.Build();
 host.Run();
 
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
-{
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        var user = new User(Guid.NewGuid(), "Alex");
-        logger.LogWarning("As User = {user}", user);
 
-        var order = Order.Create(user);
-        logger.LogWarning("Order Created = {@Order}", order);
-
-        var product = Product.Create("Test");
-        order.AddProduct(product);
-        logger.LogWarning("Order.AddProduct {@Order}", order);
-
-    }
-}
 
 
 
---
+++ Serilog Structured Logging/Worker.cs
@@ -0,0 +1,18 @@
+﻿namespace Serilog_Structured_Logging;
+
+public class Worker(ILogger<Worker> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var user = new User(Guid.NewGuid(), "Alex");
+        logger.LogWarning("As User = {user}", user);
+
+        var order = Order.Create(user);
+        logger.LogWarning("Order Created = {@Order}", order);
+
+        var product = Product.Create("Test");
+        order.AddProduct(product);
+        logger.LogWarning("Order.AddProduct {@Order}", order);
+
+    }
+}
